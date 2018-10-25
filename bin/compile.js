1f094caa5dfa118de5af778fd37e02229af551ab
Michael Cook <michael.cook@techdept.co.uk>
2018-10-25 13:47:30 +0100

    Prop immutability and rel dirs but hitting a wall

--- bin/compile.js
+++ bin/compile.js
@@ -49,7 +49,7 @@ const compile = args => {
     if (!files) return;
 
     files.forEach(path => {
-      console.log("Reading:", path);
+      console.log("\nReading:", path);
       let content = fs.readFileSync(path, "utf8");
 
       //
@@ -58,12 +58,12 @@ const compile = args => {
       //
       // Replace all ${require('./_foo.html')}
       // style occurrences with a random string
-      var data = {};
-      var reg = /\$\{require\([^)]*\)[^}]*\}/g;
-      //   console.log(reg.exec(content));
-      var result;
-      var reqList = [];
-      let props = {};
+      let data = {},
+        reg = /\$\{require\([^)]*\)[^}]*\}/g,
+        //   console.log(reg.exec(content));
+        result,
+        reqList = [],
+        props = {};
       while ((result = reg.exec(content))) {
         reqList.push({
           length: result[0].length,
@@ -71,10 +71,27 @@ const compile = args => {
           value: result[0]
         });
       }
-      reqList.reverse();
+      // console.log(reqList);
+      // reqList.reverse();
+      console.log(`
+
+      Can't remember why it was reversing
+      and moreover, maybe all the xxxHTMLLINKxxx is actually unnecessary?
+      I mean it all seems a bit bonkers.
+      So many stages, and throwing the props all the way along is mad too.
+      Might want to rewrite the whole thing (pasting in regex where necessary)
+
+      `);
       content = [content];
-      reqList.forEach(function(link) {
-        var x = content.pop();
+
+      // console.log(content);
+      // console.log(reqList);
+
+      requireKey = 0;
+      reqList.forEach((link, i) => {
+        // let requireKey = reqList.length - i; // can't be right.. erm
+        // console.log("\nlink:", link.value);
+        let x = content.pop();
         do {
           var ident = randomIdent();
         } while (data[ident]);
@@ -86,19 +103,32 @@ const compile = args => {
         content.push(x.substr(0, link.start));
         // Get any prop values
         // [ 'foo="bar"', 'baz="jizz"' ]
-        let propList = link.value.match(/\b([^\s]+)(="(^'|^"|[^\s]+)*")/gi);
+        let propList = link.value.match(/\b([^\s]+)(="(.*?)")/gi);
         if (propList) {
           propList.forEach(prop => {
             let pair = prop.split("=");
-            props[pair[0]] = pair[1].substring(1, pair[1].length - 1);
+            props[`${requireKey}-${pair[0]}`] = pair[1].substring(
+              1,
+              pair[1].length - 1
+            );
           });
+          requireKey++;
         }
       });
+
+      // console.log(content.join(" "));
+      // console.log("props:", props);
+
       content.reverse();
+
       content = content.join("");
+
+      // console.log(content);
+
       //
       // Replace all the random strings with the read file
       //
+      requireKey = 0;
       content = content.replace(/xxxHTMLLINKxxx[0-9\.]+xxx/g, match => {
         if (!data[match]) return match;
 
@@ -108,8 +138,23 @@ const compile = args => {
 
         let filePath = root + data[match];
 
-        return fs.readFileSync(filePath, "utf8");
+        let fileContents = fs.readFileSync(filePath, "utf8");
+        // Getting a bit spaghetti here but trying to pass a key through for each include,
+        // so we can use it to gather the correct props at the end
+        let matchedSomething = false;
+        fileContents = fileContents.replace(/\$\{props.[^}]+\}/g, match => {
+          matchedSomething = true;
+          let propWithKey = match.split(".");
+          propWithKey =
+            propWithKey[0] + "." + requireKey + "-" + propWithKey[1];
+          // console.log(propWithKey);
+          return propWithKey;
+        });
+        if (matchedSomething) requireKey++;
+        return fileContents;
       });
+
+      // console.log(content);
 
       content = content.replace(/\$\{props.[^}]+\}/g, match => {
         // Sometimes I like the way I code; KISS Method
@@ -117,8 +162,11 @@ const compile = args => {
           "${props.".length,
           match.length - "}".length
         );
+        // console.log("\n" + propKey, props[propKey]);
         return props[propKey] ? props[propKey] : "";
       });
+
+      // console.log(content);
 
       //
       // Minify
@@ -156,7 +204,7 @@ const compile = args => {
       filename = filename[filename.length - 1];
       if (filename.substring(0, 1) != "_") {
         // Save the file to dist
-        console.log(path.substring(args.src.length));
+        // console.log(path.substring(args.src.length));
         // let filename = path.split("/").pop();
         let filename = path.substring(args.src.length);
         let outputFilepath = args.dest + filename;
