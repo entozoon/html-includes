ad23aae108ecf7ce728d84a6aaf7140e9d3d5d8d
Michael Cook <entozoon@gmail.com>
2019-02-28 17:22:58 +0000

    By the gods, I think I've done it!

--- bin/compile.js
+++ bin/compile.js
@@ -8,7 +8,9 @@ const watch = require("node-watch"),
   // regexIncludeRel = /\$\{requireRel\([^)]*\)[^}]*\}/g,
   // regexIncludeFilePath = /\$\{require\((^\))+\)/g,
   regexIncludeFilePath = /\${require\(\'(.*?)\'\)/,
-  regexFilesId = /\${filesId\((.*?)\)\}/,
+  regexFilesId = /\${filesId\((.*?)\)[^}]*\}/,
+  regexPropAttrs = /\b([^\s]+)="[^\"]*"/gi,
+  regexPropUsage = /\$\{props.[^}]+\}/g,
   maxNestedDepth = 99;
 
 // Grab CLI arguments
@@ -20,38 +22,8 @@ const options = [
 ];
 const args = commandLineArgs(options);
 
-// const getContent = (path, filename) => {
-//   // Allow relative includes. Are we having fun yet?
-//   let pathRelative = path
-//     .split("/")
-//     .slice(0, -1)
-//     .join("/");
-//   let includeFilePath = pathRelative + "/" + filename;
-
-//   let content = "";
-//   try {
-//     content = fs.readFileSync(includeFilePath, "utf8");
-//   } catch (e) {
-//     console.log("ERROR  Couldn't find file: " + includeFilePath);
-//   }
-
-//   return content;
-// };
-
 const randomIdent = () =>
   "xxxHTMLLINKxxx" + Math.random() + Math.random() + "xxx";
-
-// // e.g. ("./_sub.html", "src/index.html",) => "..../src/./_sub.html"
-// const getRelativeFilePath = (fileRequest, fileCurrent) => {
-//   console.log(fileRequest, fileCurrent);
-//   // Absolute -> rel (why not, eh?)
-//   if (fileRequest.substring(0, 1) == "/")
-//     return __dirname + "/../../../" + args.src + fileRequest;
-//   let dir = fileCurrent.split("/");
-//   dir.pop();
-//   dir = dir.join("/");
-//   return __dirname + "/../../../" + dir + "/" + fileRequest;
-// };
 
 // // e.g. ("./_sub.html", "src/index.html",) => "..../src/./_sub.html"
 const getFilesId = (fileRequest, fileCurrent, files) => {
@@ -93,27 +65,6 @@ const compile = args => {
       return { id: i, path, content: fs.readFileSync(path, "utf8") };
     });
 
-    // Try and support nested includes, here we go!
-    // let noMoreJobs = false,
-    //   loopCount = 0;
-    // // Whip round all the files, replacing any ${require()} with ${requireRel()} full path
-    // while (!noMoreJobs && loopCount < maxNestedDepth) {
-    //   noMoreJobs = true; // hopeful
-    //   files = files.map(file => {
-    //     if (file.content.match(regexInclude)) {
-    //       noMoreJobs = false; // ah well, press on
-    //       file.content = file.content.replace(regexInclude, require => {
-    //         let includeFilePath = getRelativeFilePath(
-    //           require.match(regexIncludeFilePath)[1],
-    //           file.path
-    //         );
-    //         return "${requireRel('" + includeFilePath + "')}";
-    //       });
-    //     }
-    //     return file;
-    //   });
-    //   loopCount++;
-    // }
     let noMoreJobs = false,
       loopCount = 0;
     // Whip round all the files, replacing any ${require()} with ${requireRel()} full path
@@ -128,7 +79,20 @@ const compile = args => {
               file.path,
               files
             );
-            return "${filesId(" + filesId + ")}";
+            //
+            // PROPS Passing
+            //
+            // Get any prop values and pass them through as before
+            // [ 'foo="bar"', 'baz="jizz"' ]
+            let propsAttrs = require.match(regexPropAttrs);
+            // console.log(propsAttrs);
+            return (
+              "${filesId(" +
+              filesId +
+              ")" +
+              (propsAttrs ? " " + propsAttrs.join(" ") : "") +
+              "}"
+            );
           });
         }
         return file;
@@ -144,12 +108,32 @@ const compile = args => {
         if (file.content.match(regexFilesId)) {
           noMoreJobs = false; // ah well, press on
           file.content = file.content.replace(regexFilesId, require => {
-            // Replace with it's relative file while shoving in any props *************
-            // let includeFilePath = require.match(regexIncludeFilePathRel)[1];
-            // console.log(includeFilePath);
             let filesId = require.match(regexFilesId)[1];
             // Get content from (mutated) files array (preeeetty sure it must exist)
             let filesContent = files.filter(f => f.id == filesId)[0].content;
+            //
+            // PROPS Injection
+            //
+            // Get props inline attributes
+            let propsAttrs = require.match(regexPropAttrs);
+            if (propsAttrs) {
+              let props = [];
+              // Convert props into a usable array
+              propsAttrs.forEach(prop => {
+                let pair = prop.split("=");
+                props[pair[0]] = pair[1].substring(1, pair[1].length - 1);
+              });
+              // Replace any prop usages in the content with the passed prop
+              filesContent = filesContent.replace(regexPropUsage, match => {
+                // Sometimes I like the way I code; KISS Method
+                let propKey = match.substring(
+                  "${props.".length,
+                  match.length - "}".length
+                );
+                return props[propKey] ? props[propKey] : "";
+              });
+            }
+
             // return require;
             return filesContent;
           });
@@ -158,10 +142,6 @@ const compile = args => {
       });
       loopCount++;
     }
-
-    //
-    // PROPS
-    //
 
     //
     // MINIFICATION
@@ -215,124 +195,6 @@ const compile = args => {
         });
       }
     });
-
-    // files.forEach(path => {
-    //   console.log("Reading:", path);
-    //   let content = fs.readFileSync(path, "utf8");
-
-    //   //
-    //   // Shamelessly inspired by webpack's html-loader
-    //   // https://github.com/webpack-contrib/html-loader/blob/master/index.js#L80
-    //   //
-    //   // Replace all ${require('./_foo.html')}
-    //   // style occurrences with a random string
-    //   var data = {};
-    //   var result;
-    //   var reqList = [];
-    //   let props = {};
-    //   while ((result = regexInclude.exec(content))) {
-    //     reqList.push({
-    //       length: result[0].length,
-    //       start: result.index,
-    //       value: result[0]
-    //     });
-    //   }
-    //   reqList.reverse();
-    //   content = [content];
-    //   reqList.forEach(function(link) {
-    //     var x = content.pop();
-    //     do {
-    //       var ident = randomIdent();
-    //     } while (data[ident]);
-    //     // Sprinkling some myke magic to allow flexibility for adding other attributes as params
-    //     data[ident] = /\([^)]*\)/g.exec(link.value)[0].slice(2, -2);
-    //     content.push(x.substr(link.start + link.length));
-    //     content.push(ident);
-    //     content.push(x.substr(0, link.start));
-    //     // Get any prop values
-    //     // [ 'foo="bar"', 'baz="jizz"' ]
-    //     let propList = link.value.match(/\b([^\s]+)(="(^'|^"|[^\s]+)*")/gi);
-    //     if (propList) {
-    //       propList.forEach(prop => {
-    //         let pair = prop.split("=");
-    //         props[pair[0]] = pair[1].substring(1, pair[1].length - 1);
-    //       });
-    //     }
-    //   });
-    //   content.reverse();
-    //   content = content.join("");
-    //   //
-    //   // Replace all the random strings with the read file
-    //   //
-    //   content = content.replace(/xxxHTMLLINKxxx[0-9\.]+xxx/g, match => {
-    //     if (!data[match]) return match;
-
-    //     let root = path.split("/");
-    //     root.pop();
-    //     root = root.join("/") + "/";
-
-    //     let filePath = root + data[match];
-
-    //     return fs.readFileSync(filePath, "utf8");
-    //   });
-
-    //   content = content.replace(/\$\{props.[^}]+\}/g, match => {
-    //     // Sometimes I like the way I code; KISS Method
-    //     let propKey = match.substring(
-    //       "${props.".length,
-    //       match.length - "}".length
-    //     );
-    //     return props[propKey] ? props[propKey] : "";
-    //   });
-
-    //   //
-    //   // Minify
-    //   //
-    //   // --minify
-    //   if (typeof args.minify != "undefined") {
-    //     let minimizeOptions = {
-    //       removeComments: true,
-    //       removeCommentsFromCDATA: true,
-    //       removeCDATASectionsFromCDATA: true,
-    //       collapseWhitespace: true,
-    //       conservativeCollapse: false,
-    //       removeAttributeQuotes: false,
-    //       useShortDoctype: true,
-    //       keepClosingSlash: true,
-    //       minifyJS: false,
-    //       minifyCSS: true,
-    //       removeScriptTypeAttributes: true,
-    //       removeStyleTypeAttribute: true
-    //     };
-
-    //     // --minify foo bar
-    //     if (args.minify.length) {
-    //       args.minify.forEach(arg => {
-    //         arg = arg.split("=");
-    //         minimizeOptions[arg[0]] = arg[1] == "false" ? false : true;
-    //       });
-    //     }
-
-    //     content = htmlMinifier.minify(content, minimizeOptions);
-    //   }
-
-    //   // If _partial.html, don't actually output the file
-    //   let filename = path.split("/");
-    //   filename = filename[filename.length - 1];
-    //   if (filename.substring(0, 1) != "_") {
-    //     // Save the file to dist
-    //     console.log(path.substring(args.src.length));
-    //     let filename = path.substring(args.src.length);
-    //     let outputFilePath = args.dest + filename;
-    //     console.log("Saving: " + path + "-> " + outputFilePath);
-
-    //     fse.outputFile(outputFilePath, content, err => {
-    //       if (err) {
-    //         return console.log(err);
-    //       }
-    //     });
-    //   }
-    // });
   });
 };
 
